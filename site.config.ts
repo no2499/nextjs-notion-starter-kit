68592f0ea9519324a614f6bde05762dd9c32e3e9
no2499 <115699057+no2499@users.noreply.github.com>
2025-10-23 10:56:04 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '295aa28ded56809a9853e1d23aa43cc9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'tcddd',
+  domain: 'tcddd blog',
+  author: 'TC',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'tc blog description.',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
